bd35892b5ab73d425fcdfae2784558a2ae73a504
ronnienelson <87160120+ronnienelson@users.noreply.github.com>
2021-10-29 15:35:58 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -6,11 +6,13 @@ let c= b*9/5+32
 }
 
 const display= document.querySelector('h1')
+
 const display2= document.querySelector('#high')
 const display3= document.querySelector('#low')
 const display4= document.querySelector('#condition')
-const form= document.querySelector('form')
 
+
+const form= document.querySelector('form')
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault()
@@ -27,15 +29,15 @@ form.addEventListener('submit',(e)=>{
   .then(data => {
    const tem=data.main.temp
    console.log(data)
-   //if statements that print different things to the DOM depending on the weather outside
-   if(converter(tem)>=36){
+   //if statements handle potential outcomes and display them on the DOM
+   if(converter(tem)>=36&& converter(tem)<=82 ){
    display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. It's not too fucking hot, and not too fucking cold`
    display2.innerText=`High:${converter(data.main.temp_max)}℉`
    display3.innerText=`Low:${converter(data.main.temp_min)}℉`
    display4.innerText=`Expect: ${data.weather[0].description}`
 
    }
-   else if(converter(tem)>=83){
+   else if(converter(tem)>82){
        display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. It's FUCKING HOT HOLY SHITBALLS.`
        display2.innerText=`High:${converter(data.main.temp_max)}℉`
    display3.innerText=`Low:${converter(data.main.temp_min)}℉`
