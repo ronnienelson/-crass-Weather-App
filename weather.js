160dc4c8a4c66af98cfe0aa1f62ae6f1ebfd609c
ronnienelson <87160120+ronnienelson@users.noreply.github.com>
2021-10-29 15:21:23 -0500

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,61 @@
+const converter=(a)=>{
+  let b=a-273.15
+let c= b*9/5+32
+  return Math.floor(c)
+}
+
+const display= document.querySelector('h1')
+
+const display2= document.querySelector('#high')
+const display3= document.querySelector('#low')
+const display4= document.querySelector('#condition')
+
+
+const form= document.querySelector('form')
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const input=form.elements.ins.value
+  const input2=form.elements.ins2.value
+  const input3=form.elements.ins3.value
+  const APIKEY= '29a7fe06e62c2f9cac4afa909dc55ebe'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${input},${input2},${input3}&appid=${APIKEY}`;
+
+  //api.openweathermap.org/data/2.5/weather?q=${input},${input2},${input3}&appid=${APIKEY}
+  
+ fetch(url)
+  .then(response => response.json())
+  .then(data => {
+   const tem=data.main.temp
+   console.log(data)
+   if(converter(tem)>=36){
+   display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. It's not too fucking hot, and not too fucking cold`
+   display2.innerText=`High:${converter(data.main.temp_max)}℉`
+   display3.innerText=`Low:${converter(data.main.temp_min)}℉`
+   display4.innerText=`Expect: ${data.weather[0].description}`
+
+   }
+   else if(converter(tem)>=83){
+       display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. It's FUCKING HOT HOLY SHITBALLS.`
+       display2.innerText=`High:${converter(data.main.temp_max)}℉`
+   display3.innerText=`Low:${converter(data.main.temp_min)}℉`
+   display4.innerText=`Expect: ${data.weather[0].description}`
+   }
+   else if(converter(tem)<=35){
+     display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. OH MY FUCKING GOD IT'S FUCKING FREEZING GO INSIDE.`
+     display2.innerText=`High:${converter(data.main.temp_max)}℉`
+   display3.innerText=`Low:${converter(data.main.temp_min)}℉`
+   display4.innerText=`Expect: ${data.weather[0].description}`
+   }
+   else{
+      display.innerText=`The Temperature in ${input} right now is ${converter(tem)}℉. It's a little goddamn chilly`
+   display2.innerText=`High:${converter(data.main.temp_max)}℉`
+   display3.innerText=`Low:${converter(data.main.temp_min)}℉`
+   display4.innerText=`Expect: ${data.weather[0].description}`
+   }
+  })
+  .catch(err => {
+   console.log('no',err)
+  });
+
+})
